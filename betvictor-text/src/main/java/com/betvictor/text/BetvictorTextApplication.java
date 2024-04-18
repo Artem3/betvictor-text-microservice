88130715c857d873f95e062040bac1b4b4e2f72e
Artem Goncharenko <artem.goncharenko@gmail.com>
2024-04-18 15:43:20 +0200

    init commit

---
+++ betvictor-text/src/main/java/com/betvictor/text/BetvictorTextApplication.java
@@ -0,0 +1,13 @@
+package com.betvictor.text;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetvictorTextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetvictorTextApplication.class, args);
+	}
+
+}
---
+++ betvictor-text/src/test/java/com/betvictor/text/BetvictorTextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betvictor.text;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetvictorTextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
